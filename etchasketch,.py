2691e2caab4a4ca20e254c49ffd5bed7ae3c0575
jenobles <jenobles@gmail.com>
2024-04-24 06:57:54 -0400

    testing

---
+++ etchasketch,.py
@@ -0,0 +1,30 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def clockwise():
+    tim.right(10)
+
+def counter_clockwise():
+    tim.left(10)
+
+def clear_screen():
+    tim.reset()
+
+screen.listen()
+
+screen.onkey(key = "w", fun = move_forwards)
+screen.onkey(key = "s", fun = move_backwards)
+screen.onkey(key = "d", fun = clockwise)
+screen.onkey(key = "a", fun = counter_clockwise)
+screen.onkey(key = "c", fun = clear_screen)
+
+screen.exitonclick()
